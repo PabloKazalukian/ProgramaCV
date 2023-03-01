5986b9af1989a352a346581da03e8e93fe514367
pablo <kazalukianpablo@gmail.com>
2023-02-28 21:37:05 -0300

    avatar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const yearFooter = document.getElementById("yearFooter");
-const inputText =
-  "bg-gradient-steel-c border-steel-70 text-gray-900 rounded-lg focus:border-sky-600  focus:ring-sky-600 focus:border-blue-500 block w-72 p-2.5 dark:bg-gray-700 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500";
 
 const inputName = document.getElementById("inputName");
+const inputEmail = document.getElementById("inputEmail");
+const inputMessage = document.getElementById("inputMessage");
+
 const contact_form = document.getElementById("contactForm");
+
+function addStylesInputs() {
+  inputEmail.classList.add("input");
+  inputName.classList.add("input");
+  inputMessage.classList.add("input");
+}
 
 contact_form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,16 +19,22 @@ contact_form.addEventListener("submit", (e) => {
   const message = contact_form["inputName"].value;
 
   const text = `
-    Gracias por el mensaje ${name}
+    Gracias por el mensaje ${name}.
+    Email:${email}.
+    
+    Su mensaje: ${message}
+
+    Sera enviado 
+    Muchas Gracias!!
     `;
   alert(text);
-  name = "";
-  email = "";
-  message = "";
+  contact_form.reset();
 });
 
 function getYear() {
   return new Date().getFullYear();
 }
 
+//ejecution
+addStylesInputs();
 yearFooter.innerHTML = "© " + getYear();
